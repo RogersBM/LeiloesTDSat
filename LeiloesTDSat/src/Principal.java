7f02685d5af37e88e1920354ca7d24aeeece5821
Rudy Rogers <rogersrei@gmail.com>
2024-12-24 14:58:02 -0300

    Definida a tela inicial do sistema

---
+++ LeiloesTDSat/src/Principal.java
@@ -0,0 +1,12 @@
+
+
+/**
+ *
+ * @author Rudy Rogers
+ */
+public class Principal {
+    public static void main (String[] args){
+        cadastroVIEW tela = new cadastroVIEW();
+        tela.setVisible(true);
+    }
+}
